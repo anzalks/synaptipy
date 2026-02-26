81829f1de64c5abceb74f5eef03d534cf9baf5eb
Anzal <44459736+anzalks@users.noreply.github.com>
2026-02-26 13:34:31 +0100

    chore: bump version to 0.1.0b2 and update CHANGELOG

--- src/Synaptipy/__init__.py
+++ src/Synaptipy/__init__.py
@@ -7,7 +7,7 @@ electrophysiology data using the neo library and a Qt-based graphical interface.
 """
 
 # PEP 396 style version marker
-__version__ = "0.1.0b1"
+__version__ = "0.1.0b2"
 __author__ = "Anzal K Shahul"
 __email__ = "anzal.ks@gmail.com"
 __license__ = "AGPL-3.0-or-later"
