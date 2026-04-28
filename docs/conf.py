f7488bed13df7af8a01be10dd18b41bb16b4cb3c
Anzal <anzal.ks@gmail.com>
2026-04-28 12:08:08 +0200

    fix: bump to 0.1.1b3; harden release workflow against re-run conflicts

--- docs/conf.py
+++ docs/conf.py
@@ -59,8 +59,8 @@ try:
     release = _version
     version = _version
 except Exception:
-    version = "0.1.1b2"
-    release = "0.1.1b2"
+    version = "0.1.1b3"
+    release = "0.1.1b3"
 
 # ---------------------------------------------------------------------------
 # General configuration
