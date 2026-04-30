8dc71fe0125505227ae92d48633fc7ee568e1831
Anzal <anzal.ks@gmail.com>
2026-05-01 00:12:20 +0200

    chore: bump version to v0.1.1b5

--- docs/conf.py
+++ docs/conf.py
@@ -59,8 +59,8 @@ try:
     release = _version
     version = _version
 except Exception:
-    version = "0.1.1b4"
-    release = "0.1.1b4"
+    version = "0.1.1b5"
+    release = "0.1.1b5"
 
 # ---------------------------------------------------------------------------
 # General configuration
